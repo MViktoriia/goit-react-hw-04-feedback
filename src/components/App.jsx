f88455ded7b154fd2cc6f44481ebe7ecbd1ef444
MViktoriia <mihaleva.victoria@gmail.com>
2022-05-22 09:35:04 +0300

    add jsx js css files for feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Feedback } from "./Feedback/Feedback";
+
+
 export const App = () => {
   return (
     <div
@@ -10,6 +13,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
+      <Feedback/>
       React homework template
     </div>
   );
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export class Feedback extends Component {
+    render() {
+        return (
+            <ul>
+                <li><button type="button"></button></li>
+                <li><button type="button"></button></li>
+                <li><button type="button"></button></li>
+            </ul>
+        )
+    }
+}
